3c4c8cf97744e20f8af32c64db019f77b9e0fc9d
Rallenson <Rallenson900@gmail.com>
2023-04-16 22:56:16 -0300

    first commit

---
+++ AcademicoLTP3/Controllers/DepartamentosController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AcademicoLTP3.Data;
+using AcademicoLTP3.Models;
+
+namespace AcademicoLTP3.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private readonly AcademicoLTP3Context _context;
+
+        public DepartamentosController(AcademicoLTP3Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Departamentos
+        public async Task<IActionResult> Index()
+        {
+              return _context.Departamento != null ? 
+                          View(await _context.Departamento.ToListAsync()) :
+                          Problem("Entity set 'AcademicoLTP3Context.Departamento'  is null.");
+        }
+
+        // GET: Departamentos/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Departamento == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamento
+                .FirstOrDefaultAsync(m => m.DepartamentoId == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(departamento);
+        }
+
+        // GET: Departamentos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departamentos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DepartamentoId,Nome")] Departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(departamento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamentos/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Departamento == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamento.FindAsync(id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+            return View(departamento);
+        }
+
+        // POST: Departamentos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long? id, [Bind("DepartamentoId,Nome")] Departamento departamento)
+        {
+            if (id != departamento.DepartamentoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(departamento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DepartamentoExists(departamento.DepartamentoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamentos/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Departamento == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamento
+                .FirstOrDefaultAsync(m => m.DepartamentoId == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(departamento);
+        }
+
+        // POST: Departamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long? id)
+        {
+            if (_context.Departamento == null)
+            {
+                return Problem("Entity set 'AcademicoLTP3Context.Departamento'  is null.");
+            }
+            var departamento = await _context.Departamento.FindAsync(id);
+            if (departamento != null)
+            {
+                _context.Departamento.Remove(departamento);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DepartamentoExists(long? id)
+        {
+          return (_context.Departamento?.Any(e => e.DepartamentoId == id)).GetValueOrDefault();
+        }
+    }
+}
