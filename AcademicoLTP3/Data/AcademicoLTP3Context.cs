72415367c253ef9f0449e2ab11a60e515a481b10
Rallenson <Rallenson900@gmail.com>
2023-04-16 21:38:36 -0300

    Adicionar arquivos de projeto.

---
+++ AcademicoLTP3/Data/AcademicoLTP3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AcademicoLTP3.Models;
+
+namespace AcademicoLTP3.Data
+{
+    public class AcademicoLTP3Context : DbContext
+    {
+        public AcademicoLTP3Context (DbContextOptions<AcademicoLTP3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AcademicoLTP3.Models.Departamento> Departamento { get; set; } = default!;
+    }
+}
---
+++ AcademicoLTP3/Models/Departamento.cs
@@ -0,0 +1,8 @@
+﻿namespace AcademicoLTP3.Models
+{
+    public class Departamento
+    {
+        public long? DepartamentoId { get; set; }
+        public string? Nome { get; set; }
+    }
+}
